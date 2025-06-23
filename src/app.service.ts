b0cdf11930507f6473ec6634e987d4711a628b61
LadityaGogoi <gogoiladitya@gmail.com>
2025-06-23 14:14:43 +0530

    t

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Redrob by McKinley Rice';
+    return 'testing the changes';
   }
 }
